d48df378e66d41a43183d23b9cc92371e71194f1
anandsahuh <admin@DESKTOP-1O2OVLU>
2022-01-22 09:29:48 +0530

    first commit

--- C2TC_B7/src/Generics/Genericexample1.java
+++ C2TC_B7/src/Generics/Genericexample1.java
@@ -2,4 +2,21 @@ package Generics;
 
 public class Genericexample1 {
 
+	import java.util.ArrayList;
+
+	public static void main(String[] args) {
+			ArrayList<String> list=new ArrayList<String>();
+			list.add("India");
+			list.add("japan");
+			
+			String country=list.get(0);
+			System.out.println("element is "+ country);
+			iterator<String> itr=list.iterator();
+			while(itr.hasNext())
+				System.out.println(itr.next());
+
+		}
+
+}
+
 }
